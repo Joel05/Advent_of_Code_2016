cbf3b6bc3897538bcfb2070f6e30b879eb4557d8
Joel Rupp <e12510768@student.tuwien.ac.at>
2026-01-04 17:05:56 +0100

    slight refactoring for part 2

--- day_02.py
+++ day_02.py
@@ -1,27 +1,29 @@
 # read input
-input:list = list()
+input: list = list()
 with open("input/day_02_input.txt") as file:
     for line in file:
         input.append(line.strip())
 
-###Test-input
+# Test-input
 # input = """ULL
 # RRDDD
 # LURDL
 # UUUUD""".split("\n")
 
-def clamp(value, min_val = 0, max_val = 2):
+
+def clamp(value, min_val=0, max_val=2):
     return max(min(value, max_val), min_val)
 
-#3x3 grid
+
+# 3x3 grid
 x_coordinate = 1
 y_coordinate = 1
 
-keypad_dict:dict = {(0,0) : 1, (1,0) : 2, (2,0) : 3,
-                    (0,1) : 4, (1,1) : 5, (2,2) : 6,
-                    (0,2) : 7, (1,2) : 8, (2,2) : 9}
+keypad_dict: dict = {(0, 0): 1, (1, 0): 2, (2, 0): 3,
+                     (0, 1): 4, (1, 1): 5, (2, 2): 6,
+                     (0, 2): 7, (1, 2): 8, (2, 2): 9}
 
-numbers:str = str()
+numbers: str = str()
 
 
 for instructions in input:
@@ -36,18 +38,18 @@ for instructions in input:
             case "D":
                 y_coordinate = clamp(y_coordinate+1)
     numbers += str(keypad_dict[(x_coordinate, y_coordinate)])
-    #break
+    # break
 
 print(int(numbers))
 
-
-keypad_dict:dict = {                         (2,0) : 1,
-                                (1,1) : 2,   (2,1) : 3,   (3,1) : 4,
+# autopep8: off
+keypad_dict:dict = {                         (2,0) : 1,                        
+                                (1,1) : 2,   (2,1) : 3,   (3,1) : 4,           
                     (0,2) : 5,  (1,2) : 6,   (2,2) : 7,   (3,2) : 8, (4,2) : 9,
-                                (1,3) : "A", (2,3) : "B", (3,3) : "C",
-                                             (2,4) : "D"}
-
-#5x5 grid
+                                (1,3) : "A", (2,3) : "B", (3,3) : "C",         
+                                             (2,4) : "D"}                      
+# autopep8: on
+# 5x5 grid
 x_coordinate = 0
 y_coordinate = 2
 numbers = str()
@@ -55,38 +57,22 @@ for instructions in input:
     for instruction in instructions:
         match instruction:
             case "L":
-                if  (x_coordinate == 0 or 
-                    (x_coordinate == 1 and (y_coordinate == 1 or y_coordinate == 3)) or
-                    (x_coordinate == 2 and (y_coordinate == 0 or y_coordinate == 4))):
-                    #cant move left
-                    continue
-                else:
-                    x_coordinate -= 1  
+                new_position: tuple = (x_coordinate-1, y_coordinate)
+                if new_position in keypad_dict:
+                    x_coordinate -= 1
             case "R":
-                if  (x_coordinate == 4 or 
-                    (x_coordinate == 3 and (y_coordinate == 1 or y_coordinate == 3)) or
-                    (x_coordinate == 2 and (y_coordinate == 0 or y_coordinate == 4))):
-                    #cant move right
-                    continue
-                else:
+                new_position: tuple = (x_coordinate+1, y_coordinate)
+                if new_position in keypad_dict:
                     x_coordinate += 1
             case "U":
-                if  (y_coordinate == 0 or 
-                    (y_coordinate == 1 and (x_coordinate == 1 or x_coordinate == 3)) or
-                    (y_coordinate == 2 and (x_coordinate == 0 or x_coordinate == 4))):
-                    #cant move up
-                    continue
-                else:
+                new_position: tuple = (x_coordinate, y_coordinate-1)
+                if new_position in keypad_dict:
                     y_coordinate -= 1
             case "D":
-                if  (y_coordinate == 4 or 
-                    (y_coordinate == 3 and (x_coordinate == 1 or x_coordinate == 3)) or
-                    (y_coordinate == 2 and (x_coordinate == 0 or x_coordinate == 4))):
-                    #cant move down
-                    continue
-                else:
+                new_position: tuple = (x_coordinate, y_coordinate+1)
+                if new_position in keypad_dict:
                     y_coordinate += 1
     numbers += str(keypad_dict[(x_coordinate, y_coordinate)])
-    #break
+
 
 print(numbers)
